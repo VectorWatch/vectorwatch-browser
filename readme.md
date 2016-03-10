a24b28c28176beedfde74febef9ebaee337b7b0e
Bogdan Ripa <bogdan.ripa@vectorwatch.com>
2016-03-10 19:29:35 +0200

    * added support for process.env * added a note about http-server

--- readme.md
+++ readme.md
@@ -2,11 +2,12 @@
 
 Installation:
 * npm install -g browserify
+* npm install -g http-server
 * npm install vectorwatch-sdk (or clone it form https://github.com/VectorWatch/vectorwatch-sdk)
 * Move all files except for sample.js, sample-bunde.js and sample.html under node_modules/vectorwatch-browser/
 
-
 Usage:
 * browserify sample.js -o sample-bundle.js
-* Open sample.html in your browser
+* http-server .
+* Open http://localhost:8080/sample.html in your browser
 * Open the console, click the two buttons
--- sample.js
+++ sample.js
@@ -1,18 +1,18 @@
 var VectorWatch = require('vectorwatch-browser');
 
 var vectorWatch = new VectorWatch({
-  streamUID: "ENTER_YOUR_STREAM_ID",
-  token: "ENTER_YOUR_TOKEN"
+	streamUID: process.env.STREAM_UID,
+	token: process.env.VECTOR_TOKEN
 });
 
 vectorWatch.on('subscribe', function(event, response) {
-  response.setValue(event.getUserSettings().settings['Text']);
-  response.send();
+	response.setValue(event.getUserSettings().settings['Text']);
+	response.send();
 });
 
 vectorWatch.on('config', function(event, response) {
 	var text = response.createAutocomplete('Text');
-  text.setType('INPUT_LIST');
+	text.setType('INPUT_LIST');
 	var option = text.addOption('name', 'Hello world!');
 	text.setDefaultOption(option);
 
